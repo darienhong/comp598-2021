2a9eb2107a22d99756567b9679e1c7084333dc47
Darien Hong <darienlake2009@hotmail.com>
2021-11-01 12:42:08 -0400

    collected reddit data

---
+++ hw6/submission_template/src/compute_title_lengths.py
@@ -0,0 +1,32 @@
+''' 
+Compute the average title length given a json file of reddit posts
+'''
+
+import pandas as pd 
+import json 
+import sys 
+
+def load_data(file):
+    # load json 
+    with open(file) as f: 
+        lines = f.read().splitlines()
+
+    # load each line into pandas df 
+    df_temp = pd.DataFrame(lines)
+    df_temp.columns = ['json']
+
+    # apply json loads function and normalize to convert into a flat table 
+    df = pd.json_normalize(df_temp['json'].apply(json.loads))
+
+    return df['data.title']
+
+def main(): 
+    file = sys.argv[1] 
+    df = load_data(file)
+
+    # print the mean post title length
+    print(df.str.len().mean())
+
+
+if __name__ == "__main__": 
+    main()
